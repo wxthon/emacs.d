a46a7be8d858241578915dd59227eb751c0c87c3
wxthon <wxthon@gmail.com>
2019-01-06 20:22:38 +0800

    fix theme error
    Signed-off-by: wxthon <wxthon@gmail.com>

--- layman/layman-theme.el
+++ layman/layman-theme.el
@@ -8,8 +8,7 @@
 		;;(require 'atom-one-dark-theme)
         ;;(require 'solarized-light-theme)
 		;;(require 'solarized-dark-theme)
-		(require 'doom-molokai-theme)
-    )
+		(require 'doom-molokai-theme))
     (progn
       ;;(require 'zenburn-theme)
       ;;(require 'atom-one-dark-theme)
@@ -17,5 +16,5 @@
       ;;(require 'solarized-dark-theme)
       (require 'doom-molokai-theme)
     )
-)
+))
 (layman/register-hook 'common 'layman/set-theme)
